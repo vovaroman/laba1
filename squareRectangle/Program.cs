8c7bde284f4d53b6cb6ec00feb8c856e10f191e9
vovaroman <vovan.roman7@gmail.com>
2018-10-04 17:46:13 +0300

    added readme

---
+++ squareRectangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace squareRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c ,x1, x2, d;
+            Console.WriteLine("Введите значения a b c через пробел: \n Ex:1 2 3");
+            string variables = Console.ReadLine();
+            string[] data = variables.Split(' ');
+            a = double.Parse(data[0]);
+            b = double.Parse(data[1]);
+            c = double.Parse(data[2]);
+            d = b * b - 4 * a * c;
+            if (d > 0)
+            {
+                x1 = (-b - Math.Sqrt(d)) / 2;
+                x2 = (-b + Math.Sqrt(d)) / 2;
+                Console.WriteLine(string.Format(" x1 = {0} \n" +
+                                            " x2 = {1} \n " +
+                                            " d = {2} ", x1, x2, d));
+            }
+            else if (d == 0)
+            {
+                x1 = (-b / 2 * a);
+                Console.WriteLine(string.Format(" x = {0} \n" +
+                                            " d = {1} ", x1, d));
+            }
+            else{
+                Console.Write(" d < 0... sorry...");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
